754d5fccfe3c058e91c1cb7c890bfa7856d23e80
Feustey <stephane@token-for-good.com>
2025-03-21 01:25:46 +0100

    refactor: Configuration du projet pour WebAssembly et ajout de lib.rs

---
+++ frontend/src/lib.rs
@@ -0,0 +1,10 @@
+// Définition des modules
+pub mod pages;
+pub mod components;
+pub mod services;
+pub mod types;
+
+// Fonction d'initialisation
+pub fn init() {
+    // Code d'initialisation ici
+} 
